b68ca6fb65f7eef65373d828ddb4b7081c651102
lizheng <719446602@qq.com>
2025-10-18 19:02:53 +0800

    Add files via upload

---
+++ MyLibrary/Sources/MyLibrary/MyLibrary.swift
@@ -0,0 +1,12 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+
+public struct Test {
+    public init() {}
+    public var text = "test"
+    
+    public static func testPrint() {
+        print("ssssssssssssss")
+    }
+}
